f3a13ad489485e681dbb46e538843f069e2a7ba3
Priit Laes <plaes@plaes.org>
2022-02-18 23:36:39 +0200

    Implement support for number literals

--- lox/Scanner.java
+++ lox/Scanner.java
@@ -76,9 +76,28 @@ class Scanner {
 			case '"': string(); break;
 
 			default:
-				  Lox.error(line, "Unexpected character.");
+				  if (isDigit(c)) {
+					  number();
+				  } else {
+					  Lox.error(line, "Unexpected character.");
+				  }
 				  break;
 		}
+	}
+
+	private void number() {
+		while (isDigit(peek())) advance();
+
+		// Look for a fractional part.
+		if (peek() == '.' && isDigit(peekNext())) {
+			// Consume the "."
+			advance();
+
+			while (isDigit(peek())) advance();
+		}
+
+		addToken(NUMBER,
+			Double.parseDouble(source.substring(start, current)));
 	}
 
 	private void string() {
@@ -111,6 +130,15 @@ class Scanner {
 	private char peek() {
 		if (isAtEnd()) return '\0';
 		return source.charAt(current);
+	}
+
+	private char peekNext() {
+		if (current + 1 >= source.length()) return '\0';
+		return source.charAt(current + 1);
+	}
+
+	private boolean isDigit(char c) {
+		return c >= '0' && c <= '9';
 	}
 
 	private boolean isAtEnd() {
