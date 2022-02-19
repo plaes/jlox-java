818d9124b96441c31a235f2e018c5f57b195fe70
Priit Laes <plaes@plaes.org>
2022-02-19 13:52:55 +0200

    Add support for identifiers

--- lox/Scanner.java
+++ lox/Scanner.java
@@ -78,11 +78,19 @@ class Scanner {
 			default:
 				  if (isDigit(c)) {
 					  number();
+				  } else if (isAlpha(c)) {
+					  identifier();
 				  } else {
 					  Lox.error(line, "Unexpected character.");
 				  }
 				  break;
 		}
+	}
+
+	private void identifier() {
+		while (isAlphaNumeric(peek())) advance();
+
+		addToken(IDENTIFIER);
 	}
 
 	private void number() {
@@ -135,6 +143,16 @@ class Scanner {
 	private char peekNext() {
 		if (current + 1 >= source.length()) return '\0';
 		return source.charAt(current + 1);
+	}
+
+	private boolean isAlpha(char c) {
+		return  (c >= 'a' && c <= 'z') ||
+			(c >= 'A' && c <= 'Z') ||
+			 c == '_';
+	}
+
+	private boolean isAlphaNumeric(char c) {
+		return isAlpha(c) || isDigit(c);
 	}
 
 	private boolean isDigit(char c) {
